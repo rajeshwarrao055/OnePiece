b26daf231128c36966ec3145563f63c1f4b39485
Rajeshwar <rajeshwarrao055@gmail.com>
2023-05-06 21:47:45 +0530

    Rajeshwar | Boostrapping repository

---
+++ src/main/java/com/personal/OnePiece/OnePieceApplication.java
@@ -0,0 +1,13 @@
+package com.personal.OnePiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePieceApplication.class, args);
+	}
+
+}
